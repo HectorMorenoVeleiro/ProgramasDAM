e4ef6ad3ce71432d320c226b6f61347e306765fb
Hector <hmorenoveleiro@gmail.com>
2025-11-17 12:00:21 +0100

    .

---
+++ ProgramasPythonDAM/EjerciciosLibro/Ejercicio1.py
@@ -0,0 +1,32 @@
+# ---------------------------------------------------------------------
+# Introducción --
+# ---------------------------------------------------------------------
+# En esta relación se plantean ejercicios con definiciones de funciones
+# por composición sobre números, listas y booleanos.
+# ----------------------------------------------------------------------
+# Cabecera
+# ----------------------------------------------------------------------
+from math import pi
+from typing import TypeVar
+from hypothesis import given
+from hypothesis import strategies as st
+
+A = TypeVar("A")
+
+
+# ---------------------------------------------------------------------
+# Ejercicio 1. Definir la función
+# media3 : (float, float, float) -> float
+# tal que (media3 x y z) es la media aritmética de los números x, y y
+# z. Por ejemplo,
+# media3(1, 3, 8) == 4.0
+# media3(-1, 0, 7) == 2.0
+# media3(-3, 0, 3) == 0.0
+# ---------------------------------------------------------------------
+def media3(x: float, y: float, z: float) -> float:
+    return (x + y + z) / 3
+
+
+for i in range(1, 6):
+    contador = 0
+    contador = contador + 1
