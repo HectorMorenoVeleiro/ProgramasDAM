db912ebe3b1f17d11bdfbb8b581b0cf7d605c47c
Hector <hmorenoveleiro@gmail.com>
2025-11-18 09:54:01 +0100

    .

--- ProgramasJavaDAM/src/cuatro/NumerosAmigos.java
+++ ProgramasJavaDAM/src/cuatro/NumerosAmigos.java
@@ -2,7 +2,15 @@ package cuatro;
 
 public class NumerosAmigos {
 
-    public static int sumaDivisoresP(int entero) { // metodo para saber la suma de los divisores de un numero
+    private int entero1;
+    private int entero2;
+
+    public NumerosAmigos(int entero1, int entero2) {
+        this.entero1 = entero1;
+        this.entero2 = entero2;
+    }
+
+    public int sumaDivisoresP(int entero) { // metodo para saber la suma de los divisores de un numero
         int suma = 0;
         for (int i = 1; i < entero; i++)
             if (entero % i == 0)
@@ -10,8 +18,8 @@ public class NumerosAmigos {
         return suma;
     }
 
-    public static boolean sonAmigos(int entero1, int entero2) { // metodo para comparar la suma de los divisores de 1
-                                                                // numero con el otro y viceversa
+    public boolean sonAmigos() { // metodo para comparar la suma de los divisores de 1
+                                 // numero con el otro y viceversa
         if (entero1 == sumaDivisoresP(entero2) && entero2 == sumaDivisoresP(entero1))
             return true;
         else
@@ -19,6 +27,7 @@ public class NumerosAmigos {
     }
 
     public static void main(String[] args) {
-        System.out.println("Are they friends? --> " + sonAmigos(284, 220));
+        NumerosAmigos numAmig1 = new NumerosAmigos(220, 284);
+        System.out.println(numAmig1.sonAmigos());
     }
 }
