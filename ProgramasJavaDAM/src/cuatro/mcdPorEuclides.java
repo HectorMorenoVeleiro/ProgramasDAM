e9f7b04d34f6985f52e67ea8882e79c89678ea2c
Hector <hmorenoveleiro@gmail.com>
2025-11-18 17:42:37 +0100

    .

---
+++ ProgramasJavaDAM/src/cuatro/mcdPorEuclides.java
@@ -0,0 +1,29 @@
+package cuatro;
+
+public class mcdPorEuclides {
+
+    // iniciar atributos
+    private int numero1;
+    private int numero2;
+
+    // método constructor
+    public mcdPorEuclides(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    // método sobrecargado para acceder desde el objeto creado
+    public int calcularMcdNumero() {
+        return calcularMcdNumero(this.numero1, this.numero2);
+    }
+
+    // método que calcula el mcd entre 2 numeros que le entregues
+    public int calcularMcdNumero(int numero1, int numero2) {
+        if (numero2 == 0)
+            return numero1;
+        else {
+            int resto = numero1 % numero2;
+            return calcularMcdNumero(numero2, resto);
+        }
+    }
+}
