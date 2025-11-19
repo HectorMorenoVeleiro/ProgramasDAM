2f7df5016fdf05cf1ef0b56be713f579791ac7f7
Hector <hmorenoveleiro@gmail.com>
2025-11-19 14:34:14 +0100

    .

--- ProgramasJavaDAM/src/cuatro/BiSiesta.java
+++ ProgramasJavaDAM/src/cuatro/BiSiesta.java
@@ -4,15 +4,17 @@ public class BiSiesta {
     // La regla para determinar un año bisiesto es que el año debe ser divisible por
     // 4, pero los años divisibles por 100 no son bisiestos a menos que también sean
     // divisibles por 400.
+
+    // atributos
     private int año;
 
+    // método constructor
     public BiSiesta(int año) {
         this.año = año;
     }
 
+    // métoco para calcular si esBisiesto
     public boolean esBisiesto() {
-        if (this.año % 4 == 0 || this.año % 100 == 0 && this.año % 400 == 0)
-            return true;
-        return false;
+        return this.año % 4 == 0 || (this.año % 100 == 0 && this.año % 400 == 0);
     }
 }
