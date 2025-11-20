dfe98c2795e5ba60e812fb0e80547748dad051ef
Hector <hmorenoveleiro@gmail.com>
2025-11-20 12:48:15 +0100

    .

--- ProgramasJavaDAM/src/cuatro/EjercicioExamen2.java
+++ ProgramasJavaDAM/src/cuatro/EjercicioExamen2.java
@@ -2,4 +2,21 @@ package cuatro;
 
 public class EjercicioExamen2 {
 
+    // atributos
+    private double nota;
+
+    private final
+
+    // método constructor
+    public EjercicioExamen2(double nota) {
+        this.nota = nota;
+    }
+
+    public String comprobarCalificacion(){
+        while(nota < 0.0 || nota > 10.0){
+            if ()
+        }
+        return "si";
+    }
+
 }
