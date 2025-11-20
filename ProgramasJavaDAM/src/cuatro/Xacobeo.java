d45906921b03ac3bd76788439b80cc6aca51a708
Hector <hmorenoveleiro@gmail.com>
2025-11-20 11:48:22 +0100

    .

---
+++ ProgramasJavaDAM/src/cuatro/Xacobeo.java
@@ -0,0 +1,14 @@
+package cuatro;
+
+public class Xacobeo {
+
+    private int año;
+
+    public Xacobeo(int año) {
+        this.año = año;
+    }
+
+    public boolean esXacobeo() {
+        return true;
+    }
+}
