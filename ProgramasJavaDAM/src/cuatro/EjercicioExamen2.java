61065598dcb44260afb5f0a8adb01ca7d2d7e18e
Hector <hmorenoveleiro@gmail.com>
2025-11-20 12:51:00 +0100

    .

--- ProgramasJavaDAM/src/cuatro/EjercicioExamen2.java
+++ ProgramasJavaDAM/src/cuatro/EjercicioExamen2.java
@@ -5,18 +5,27 @@ public class EjercicioExamen2 {
     // atributos
     private double nota;
 
-    private final
+    private final String[] recorridoNota = { "SUSPENSO, SUFICIENTE, BIEN, NOTABLE, SOBRESALIENTE" };
 
     // método constructor
     public EjercicioExamen2(double nota) {
         this.nota = nota;
     }
 
-    public String comprobarCalificacion(){
-        while(nota < 0.0 || nota > 10.0){
-            if ()
+    public String comprobarCalificacion() {
+        while (nota < 0.0 || nota > 10.0) {
+            if (nota >= 9)
+                return recorridoNota[4];
+            else if (nota >= 7)
+                return recorridoNota[3];
+            else if (nota >= 6)
+                return recorridoNota[2];
+            else if (nota >= 5)
+                return recorridoNota[1];
+            else
+                return recorridoNota[0];
         }
-        return "si";
+        return "Fantasmón";
     }
 
 }
