271e7d6b103658dd25c3e5f61eca88adf937fb88
Hector <hmorenoveleiro@gmail.com>
2025-11-24 09:51:34 +0100

    .

--- ProgramasJavaDAM/src/cuatro/ArrayMaxMin.java
+++ ProgramasJavaDAM/src/cuatro/ArrayMaxMin.java
@@ -1,6 +1,7 @@
 package cuatro;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 
 public class ArrayMaxMin {
 
@@ -18,5 +19,32 @@ public class ArrayMaxMin {
             if (entero < min)
                 min = entero;
         }
+
+        System.out.println(max);
+        System.out.println(min);
     }
+
+    public static void encontrarMaxMinConStream(int[] array) {
+
+        // 1. Verificacion basica
+        if (array == null || array.length == 0) {
+            System.out.println("El array esta vacio o es nulo");
+            return;
+        }
+
+        // 2. Crear un IntStream a partir del array
+        OptionalInt maximo = Arrays.stream(array).max();
+        OptionalInt minimo = Arrays.stream(array).min();
+
+        // 3. Obtener resultados de OptionalInt
+        // Usamos isPresent() y getAsInt() para manejar el caso de array vacio(aunque ya
+        // lo comprobamos previamente)
+        if (maximo.isPresent()) {
+            System.out.println("--- Resultados (Usando Streams) ---");
+            System.out.println("El valor Maximo es: " + maximo.getAsInt());
+            System.out.println("El valor Maximo es: " + minimo.getAsInt());
+        }
+
+    }
+
 }
