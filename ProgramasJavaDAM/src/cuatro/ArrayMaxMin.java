8f7d556295c7ba31e916868530db435512b67251
Hector <hmorenoveleiro@gmail.com>
2025-11-24 09:30:25 +0100

    .

--- ProgramasJavaDAM/src/cuatro/ArrayMaxMin.java
+++ ProgramasJavaDAM/src/cuatro/ArrayMaxMin.java
@@ -3,6 +3,10 @@ package cuatro;
 public class ArrayMaxMin {
 
     public static void main(String[] args) {
+        int[] datos = { 1, 2, 3, 3, 4, 4, 5, 2 };
+        int largo = datos.length;
+        for (Integer entero : datos) {
 
+        }
     }
 }
