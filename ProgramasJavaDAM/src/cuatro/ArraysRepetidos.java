bcb4a2135cea2e2a5fdd3b815ce0a78b268bfab9
Hector <hmorenoveleiro@gmail.com>
2025-11-24 09:08:20 +0100

    .

---
+++ ProgramasJavaDAM/src/cuatro/ArraysRepetidos.java
@@ -0,0 +1,9 @@
+package cuatro;
+
+public class ArraysRepetidos {
+    public static void main(String[] args) {
+        int[] datos = { 1, 2, 3, 3, 4, 4, 5, 2 };
+        int largo = datos.length;
+        System.out.print("longitud del array -> " + largo);
+    }
+}
