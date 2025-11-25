5903010ab638b5e2b8cb923a198a91c84ddd1f9b
Hector <hmorenoveleiro@gmail.com>
2025-11-25 09:39:38 +0100

    .

--- ProgramasJavaDAM/src/cuatro/DesigualdadTriangular.java
+++ ProgramasJavaDAM/src/cuatro/DesigualdadTriangular.java
@@ -2,22 +2,19 @@ package cuatro;
 
 public class DesigualdadTriangular {
 
+    // atributos
     double ladoA;
     double ladoB;
     double ladoC;
 
-    DesigualdadTriangular() {
-        this.ladoA = 0;
-        this.ladoB = 0;
-        this.ladoC = 0;
-    }
-
+    // constructor por parametros
     DesigualdadTriangular(double ladoA, double ladoB, double ladoC) {
         this.ladoA = ladoA;
         this.ladoB = ladoB;
         this.ladoC = ladoC;
     }
 
+    // metodo para saber si es triangulo o no
     public boolean isTriangle() {
         return (this.ladoA <= (this.ladoB + this.ladoC) && this.ladoC <= (this.ladoA + this.ladoB)
                 && this.ladoB <= (this.ladoA + this.ladoC));
