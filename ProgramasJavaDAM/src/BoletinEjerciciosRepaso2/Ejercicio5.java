45c3bb0efbc94b394c057fd657aff68bc8ba0ba8
Hector <hmorenoveleiro@gmail.com>
2025-11-26 14:13:24 +0100

    .

--- ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio5.java
+++ ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio5.java
@@ -11,39 +11,44 @@ public class Ejercicio5 {
         Scanner sc = new Scanner(System.in);
         System.out.println("que quieres haer ? 1/2");
         int decision = Integer.parseInt(sc.nextLine());
-        if (decision == 1) {
-            System.out.println("introduce el numero a adivinar");
-            int numeroAdivinar = Integer.parseInt(sc.nextLine());
-            int limiteTop = 100;
-            int limiteBot = 0;
-            while (true) {
-                int numero = (int) ((Math.random() * (limiteTop - limiteBot) + limiteBot));
-                if (numero == numeroAdivinar)
-                    break;
-                if (numero > numeroAdivinar) {
-                    limiteTop = numero;
-                    System.out.println(numero);
-                    System.out.println("el numero es menor");
+        while (true) {
+            if (decision == 1) {
+                System.out.println("introduce el numero a adivinar");
+                int numeroAdivinar = Integer.parseInt(sc.nextLine());
+                int limiteTop = 100;
+                int limiteBot = 0;
+                while (true) {
+                    int numero = (int) ((Math.random() * (limiteTop - limiteBot) + limiteBot));
+                    if (numero == numeroAdivinar)
+                        break;
+                    if (numero > numeroAdivinar) {
+                        limiteTop = numero;
+                        System.out.println(numero);
+                        System.out.println("el numero es menor");
+                    }
+                    if (numero < numeroAdivinar) {
+                        System.out.println(numero);
+                        limiteBot = numero;
+                        System.out.println("el numero es mayor");
+                    }
+                    sc.nextLine();
                 }
-                if (numero < numeroAdivinar) {
-                    System.out.println(numero);
-                    limiteBot = numero;
-                    System.out.println("el numero es mayor");
+                System.out.println("acertaste");
+                break;
+            } else if (decision == 2) {
+                int numeroGenerado = (int) (Math.random() * 1000 + 1);
+                int numeroPersona = 0;
+                while (numeroGenerado != numeroPersona) {
+                    System.out.println("introduce un numero para ser adivinado");
+                    numeroPersona = Integer.parseInt(sc.nextLine());
+                    if (numeroPersona < numeroGenerado)
+                        System.out.println("el numero es mayor");
+                    else if (numeroPersona > numeroGenerado)
+                        System.out.println("el numero es menor");
                 }
-                sc.nextLine();
-            }
-            System.out.println("acertaste");
-        } else if (decision == 2) {
-            int numeroGenerado = (int) (Math.random() * 1000 + 1);
-            int numeroPersona = 0;
-            while (numeroGenerado != numeroPersona) {
-                System.out.println("introduce un numero para ser adivinado");
-                numeroPersona = Integer.parseInt(sc.nextLine());
-                if (numeroPersona < numeroGenerado)
-                    System.out.println("el numero es mayor");
-                else if (numeroPersona > numeroGenerado)
-                    System.out.println("el numero es menor");
-            }
+                break;
+            } else
+                continue;
         }
         sc.close();
     }
