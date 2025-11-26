b7eaf39a2ddab82add38a4e810a1fe71dbd0d96d
Hector <hmorenoveleiro@gmail.com>
2025-11-26 14:19:03 +0100

    .

--- ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio5.java
+++ ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio5.java
@@ -10,46 +10,73 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            System.out.println("que quieres haer ? 1/2");
+
+            System.out.println("""
+                        Que quieres hacer ?
+
+                        1. La maquina adivina tu numero
+
+                        2. Adivinas el numero de la maquina
+
+                        3. Salir
+                    __________________________________________""");
+
             int decision = Integer.parseInt(sc.nextLine());
-            if (decision == 1) {
+
+            if (decision == 1) { // tu dices el numero a adivinar y la maquina adivina
+
                 System.out.println("introduce el numero a adivinar");
                 int numeroAdivinar = Integer.parseInt(sc.nextLine());
                 int limiteTop = 1000;
                 int limiteBot = 0;
+
                 while (true) {
                     int numero = (int) ((Math.random() * (limiteTop - limiteBot) + limiteBot));
+
                     if (numero == numeroAdivinar)
                         break;
+
                     if (numero > numeroAdivinar) {
                         limiteTop = numero;
                         System.out.println(numero);
                         System.out.println("el numero es menor");
                     }
+
                     if (numero < numeroAdivinar) {
                         System.out.println(numero);
                         limiteBot = numero;
                         System.out.println("el numero es mayor");
                     }
+
                     sc.nextLine();
                 }
                 System.out.println("acertaste");
-                break;
             }
-            if (decision == 2) {
+
+            if (decision == 2) { // la maquina pone un numero y tu lo adivinas
+
                 int numeroGenerado = (int) (Math.random() * 1000 + 1);
                 int numeroPersona = 0;
+
                 while (numeroGenerado != numeroPersona) {
+
                     System.out.println("introduce un numero para ser adivinado");
                     numeroPersona = Integer.parseInt(sc.nextLine());
+
                     if (numeroPersona < numeroGenerado)
                         System.out.println("el numero es mayor");
+
                     else if (numeroPersona > numeroGenerado)
                         System.out.println("el numero es menor");
+
                 }
-                break;
             }
+
+            if (decision == 3)
+                break;
+
         }
+
         sc.close();
     }
 
