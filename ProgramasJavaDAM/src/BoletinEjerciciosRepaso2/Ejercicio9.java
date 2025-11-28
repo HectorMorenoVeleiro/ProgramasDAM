0e2e5427669f83a808f5f624c704a7944393d63a
Hector <hmorenoveleiro@gmail.com>
2025-11-28 09:52:03 +0100

    .

---
+++ ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio9.java
@@ -0,0 +1,11 @@
+package BoletinEjerciciosRepaso2;
+
+public class Ejercicio9 {
+    // Escribir todos los numeros del 100 al 0 de 7 en 7
+    public static void main(String[] args) {
+        for (int i = 100; i > 0; i = i - 7) {
+            System.out.println("numero " + i);
+        }
+    }
+
+}
