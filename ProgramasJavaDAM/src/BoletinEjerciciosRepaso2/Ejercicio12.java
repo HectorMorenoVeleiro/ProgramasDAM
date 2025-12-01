982f1863d6f8abdc04d7579e10c7592437e2960e
Hector <hmorenoveleiro@gmail.com>
2025-12-01 09:27:05 +0100

    .

--- ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio12.java
+++ ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio12.java
@@ -3,11 +3,15 @@ package boletinejerciciosrepaso2;
 import java.util.Scanner;
 
 public class Ejercicio12 {
-    //
+    // Pedir un numero y calcular su factorial
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double numero = Double.parseDouble(sc.nextLine());
-        System.out.println(numero);
+        double productorio = 1;
+        for (double i = numero; i > 0; i--) {
+            productorio *= i;
+        }
+        System.out.println("el factorial del numero es -> " + productorio);
         sc.close();
     }
 }
