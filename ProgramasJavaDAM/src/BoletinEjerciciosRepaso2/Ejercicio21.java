529d844ec368883eedfc09dd5eec90e87db97938
Hector <hmorenoveleiro@gmail.com>
2025-12-04 12:45:54 +0100

    .

---
+++ ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio21.java
@@ -0,0 +1,9 @@
+package boletinejerciciosrepaso2;
+
+public class Ejercicio21 {
+    // Pedir 10 numeros y mostrar al final si se ha introducido alguno negativo
+
+    public static void main(String[] args) {
+
+    }
+}
