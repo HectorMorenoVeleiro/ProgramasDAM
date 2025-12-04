44852bf5c16e1843898906f5b968c67c6649aa2c
Hector <hmorenoveleiro@gmail.com>
2025-12-04 12:54:44 +0100

    .

--- ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio21.java
+++ ProgramasJavaDAM/src/BoletinEjerciciosRepaso2/Ejercicio21.java
@@ -1,9 +1,23 @@
 package boletinejerciciosrepaso2;
 
+import java.util.Scanner;
+
 public class Ejercicio21 {
     // Pedir 10 numeros y mostrar al final si se ha introducido alguno negativo
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean bandera = false;
+        double numero = 0;
 
+        for (int i = 0; i < 10; i++) {
+            numero = Double.parseDouble(sc.nextLine());
+            if (numero < 0)
+                bandera = true;
+        }
+
+        System.out.println("ha habido algun negativo ? -> " + bandera);
+
+        sc.close();
     }
 }
