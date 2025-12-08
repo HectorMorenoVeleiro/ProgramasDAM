8815e64783961ba272a8ff7bad97c52faecb65a7
Hector Moreno <hmorenoveleiro@gmail.com>
2025-12-08 20:13:09 +0100

    ,

---
+++ ProgramasJavaDAM/src/EjerciciosCuentaPropia/NotasAsignaturasFile.java
@@ -0,0 +1,22 @@
+package ejercicioscuentapropia;
+
+/*Realiza un programa con un cliente en el que le pidas al usuario las
+notas de las 12 asignaturas del Ciclo de DAM (nombre, curso y nota)
+y las envíe a un servidor que las recogerá, calculará la media de
+primero, la de segundo y la media total y las devolverá al cliente,
+que las mostrara.
+
+Nota: Cada una de las asignaturas serán un objeto que se encuentra
+en un arraylist, y cuyos atributos serán el nombre, el curso y la
+nota.
+
+Nota 2: Con el constructor podrás asignar directamente el nombre y
+el curso de la asignatura al crear el objeto. En cambio, el atributo
+nota, será el usuario quien lo introduzca mediante un método que
+controle que la nota tenga un valor entre 0 y 10.*/
+
+
+public static void main(String[] args) {
+	System.out.print();
+
+}
