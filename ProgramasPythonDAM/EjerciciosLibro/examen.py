371c5645c5424dd6aab90f15c77ca8c34a819c4c
Hector Moreno <hmorenoveleiro@gmail.com>
2025-12-10 17:58:01 +0100

    ,

---
+++ ProgramasPythonDAM/EjerciciosLibro/examen.py
@@ -0,0 +1,10 @@
+def fizz_buzz():
+    for i in range(0, 31):
+        if i % 3 == 0 & i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 5 != 0:
+            print("Fizz")
+        elif i % 3 != 0:
+            print("Buzz")
+        else:
+            print(i)
