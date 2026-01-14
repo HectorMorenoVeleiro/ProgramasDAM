ed344d9d933b8187e8a1ef803983cf2bb9278117
Hector <hmorenoveleiro@gmail.com>
2026-01-14 14:26:20 +0100

    .

--- ProgramasJavaDAM/src/segundo_trimestre/Cadena8.java
+++ ProgramasJavaDAM/src/segundo_trimestre/Cadena8.java
@@ -3,6 +3,8 @@ package segundo_trimestre;
 public class Cadena8 {
 
     public static void main(String[] args) {
-
+        String cad1 = "MAYKA MORENO";
+        System.out.println(cad1.startsWith("JUAN"));
+        System.out.println(cad1.startsWith("MAY"));
     }
 }
