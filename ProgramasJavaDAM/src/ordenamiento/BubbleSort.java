d60b9a5aee0e22c4feab42324117acd712e1af21
Hector <hmorenoveleiro@gmail.com>
2026-01-19 09:08:51 +0100

    .

--- ProgramasJavaDAM/src/ordenamiento/BubbleSort.java
+++ ProgramasJavaDAM/src/ordenamiento/BubbleSort.java
@@ -1,9 +1,25 @@
 package ordenamiento;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
+        int datos[] = { 2, 5, 0, 9, 3, 1, 8, 4, 7, 6, 2, 5, 9, 0, 3, 1, 4, 7, 8, 2 };
+        int swap = 0;
 
+        for (int i = 1; i < datos.length; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (datos[j] > datos[j + 1]) {
+                    swap = datos[j];
+                    datos[j] = datos[j + 1];
+                    datos[j + 1] = swap;
+                }
+            }
+        }
+
+        System.out.println(String.valueOf(swap) + " " + Arrays.toString(datos));
     }
-
 }
