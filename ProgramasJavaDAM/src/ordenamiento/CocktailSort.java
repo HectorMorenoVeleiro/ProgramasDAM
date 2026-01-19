10db39b71599bf9baab6ec1e4f5d2f78131f9ca6
Hector <hmorenoveleiro@gmail.com>
2026-01-19 10:07:32 +0100

    .

--- ProgramasJavaDAM/src/ordenamiento/CocktailSort.java
+++ ProgramasJavaDAM/src/ordenamiento/CocktailSort.java
@@ -1,5 +1,7 @@
 package ordenamiento;
 
+import java.util.Arrays;
+
 public class CocktailSort {
 
     public static int[] CocktailSortMethot(int[] numbers) {
@@ -8,7 +10,7 @@ public class CocktailSort {
         int j = numbers.length - 1;
         while (i < j && swapped) {
             swapped = false;
-            for (int k = 1; k < j; k++) {
+            for (int k = i; k < j; k++) {
                 if (numbers[k] > numbers[k + 1]) {
                     int temp = numbers[k];
                     numbers[k] = numbers[k + 1];
@@ -19,8 +21,8 @@ public class CocktailSort {
             j--;
             if (swapped) {
                 swapped = false;
-                for (int k = j; k > j; k--) {
-                    if (numbers[k] < numbers[k + 1]) {
+                for (int k = j; k > i; k--) {
+                    if (numbers[k] < numbers[k - 1]) {
                         int temp = numbers[k];
                         numbers[k] = numbers[k - 1];
                         numbers[k - 1] = temp;
@@ -28,13 +30,19 @@ public class CocktailSort {
                     }
                 }
             }
+            i++;
         }
         return numbers;
     }
 
     public static void main(String[] args) {
-
         // a.k.a. Burbuja bidireccional
+
+        int datos[] = { 2, 5, 0, 9, 3, 1, 8, 4, 7, 6, 2, 5, 9, 0, 3, 1, 4, 7, 8, 2 };
+
+        System.out.println(Arrays.toString(datos));
+
+        System.out.println(Arrays.toString(CocktailSortMethot(datos)));
 
     }
 
