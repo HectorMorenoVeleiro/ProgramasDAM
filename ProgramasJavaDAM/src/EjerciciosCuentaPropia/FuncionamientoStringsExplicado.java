5766fb1fcbcdb41091a48358ba7b7bb39cc40caf
Hector <hmorenoveleiro@gmail.com>
2026-01-20 14:34:10 +0100

    .

---
+++ ProgramasJavaDAM/src/EjerciciosCuentaPropia/FuncionamientoStringsExplicado.java
@@ -0,0 +1,183 @@
+package ejercicioscuentapropia;
+
+import java.util.Arrays;
+
+public class FuncionamientoStringsExplicado {
+
+    /*
+     * · StringBuffer -->
+     * 
+     * - El StringBuffer es una clase en Java que tiene como función
+     * manejar cadenas de texto que cambian constantemente.
+     * 
+     * - Funciona basicamente como un contenedor dinámico, que posee
+     * un arreglo interno de caracteres con una capacidad específica
+     * y en la cual, al integrar texto, lo añades directamente en ese
+     * arreglo, permitiendo que la capacidad del String se expanda
+     * automáticamente
+     *
+     */
+
+    public static void stringBuffer1() {
+
+        /*
+         * El StringBuffer tiene diversos usos, además de diversas
+         * formas de implementación al crear un objeto de esa clase.
+         * Estas formas son -->
+         * 
+         */
+
+        // Por el paso de un String al crear la cadena -->
+        StringBuffer nombre = new StringBuffer("Renso");
+
+        // Por el paso del tamaño del String, sin ninguna cadena de
+        // texto previa -->
+        StringBuffer apellidos = new StringBuffer(80);
+
+        // Sin pasarle nada, creando una cadena vacía que, al ser
+        // extensible y adaptable, se irá aumentando con la
+        // implementación de cadenas de texto -->
+        StringBuffer dirección = new StringBuffer();
+
+        // ---------------------------------------------------------
+
+        /*
+         * Ahora pasaré a mostrar los usos que puede tener el
+         * StringBuffer en general -->
+         * 
+         */
+
+        // Mirar el tamaño de la cadena que acabamos de crear como
+        // objeto -->
+        System.out.println(nombre.length());
+        System.out.println(apellidos.length());
+        System.out.println(dirección.length());
+
+        // Mirar la capacidad de la cadena que acabamos de crear
+        // como objeto -->
+        System.out.println(nombre.capacity());
+        System.out.println(apellidos.capacity());
+        System.out.println(dirección.capacity());
+
+        // Añadir una cadena de texto al final del objeto que
+        // acabas de crear -->
+        System.out.println(nombre.append(" Regino"));
+        System.out.println(nombre.append(" Moreno Perez"));
+        System.out.println(nombre.append(" Camino de las luces"));
+
+        // también podemos trabajar con el StringBuffer usando un
+        // objeto String para añadirselo a tu StringBuffer -->
+        String otrosApellidos = new String("Moreno Perez");
+
+        // Y ahora se lo añades a uno de tus objetos StringBuffer
+        // y lo mostramos por pantalla -->
+        nombre.append(otrosApellidos);
+        System.out.println(nombre);
+
+        // También se le puede dar la vuelta al String
+        nombre.reverse();
+
+        // E incluso hacerle un toString para convertirlo en una
+        // cadena de caracteres
+        nombre.toString();
+
+        // También convertimos el StringBuffer a un String simple
+        String experimentoFinalizado = nombre.toString();
+        System.out.println(experimentoFinalizado);
+
+    }
+
+    public static void stringBuffer2() {
+
+        /*
+         * Ya sabemos como trabajar ligeramente con StringBuffer's,
+         * pero aun quedan muchas cosas por aprender, por ejemplo
+         * como modificar las propias cadenas del String -->
+         *
+         */
+
+        // Primero que nada crearemos un StringBuffer objeto nombre,
+        // con otro String apellidos que le implementaremos mas
+        // adelante -->
+        StringBuffer nombre = new StringBuffer("EMMA");
+        String apellidos = new String("MORENO");
+
+        // Ahora añadimos el String "apellidos" al objeto StringBuffer
+        // "nombre", para que quede el nombre con los apellidos dentro
+        // del mismo objeto, y lo mostramos por pantalla -->
+        nombre.insert(nombre.length(), apellidos); // introducimos String + tamaño
+        System.out.println(nombre);
+
+        // También podemos crear un StringBuffer nuevo y eliminarle partes
+        // del mismo en base a su index (su tamaño + su longitud), y
+        // suprimirá esa parte de la cadena -->
+        StringBuffer nombre2 = new StringBuffer("RAUL JESUS TURRION");
+        nombre2 = nombre2.delete(4, 10);
+        System.out.println(nombre);
+
+        // O por ejemplo decidir sustituir o darle a vuelta al StringBuffer
+        // para que en vez de poner nombre y apellidos ponta apellidos y
+        // nombre -->
+        StringBuffer nombre3 = new StringBuffer("RAUL JESUS TURRION");
+        String turri = nombre3.substring(0, 4) + nombre3.substring(10);
+        System.out.println("y sale: " + turri);
+
+        // Por ultimo saber que siempre puedes mostrar un StringBuffer como
+        // una cadena de caracteres pasada a un toString -->
+        StringBuffer nombre4 = new StringBuffer("TURRION");
+        String cadena = nombre4.toString();
+        System.out.println(cadena);
+
+    }
+
+    public static void cadenas1() {
+
+        /*
+         * Aquí trabajaremos con el uso de cadenas de caracteres en forma de
+         * Strings, para ver como manejarlos y las propiedades que tiene
+         * también -->
+         * 
+         */
+
+        // Podemos crear una cadena de caracteres en base a los propios
+        // caracteres -->
+        char[] nombre1 = { 'p', 'e', 'p', 'e' };
+
+        // O también lo podemos hacer en base a su index ASCII -->
+        char[] nombre2 = { 112, 101, 112, 101 };
+
+        // O le podemos dar un espacio en memoria pero siendo vacio -->
+        char[] nombre3 = new char[4];
+
+        // Evidentemente los mostraremos por pantalla
+        System.out.println(Arrays.toString(nombre1)); // p e p e
+        System.out.println(Arrays.toString(nombre2)); // p e p e
+        System.out.println(Arrays.toString(nombre3)); // 0 0 0 0
+
+        // -----------------------------------------------------------------
+
+        /*
+         * Ahora vamos a mostrar como se pueden convertir estos caracteres a
+         * objetos String para un uso mas interesante -->
+         * 
+         */
+
+        // Pasándolo como objeto para trabajar con el -->
+        String cadena1 = new String(nombre1);
+        System.out.println(cadena1);
+
+        // Creando un objeto pero dandole el valor de String de la
+        // cadena -->
+        String cadena2 = String.valueOf(nombre2);
+        System.out.println(cadena2);
+
+        // By Looping&Adding
+        String cadena3 = ""; // Empty String
+        for (int i = 0; i < nombre3.length; i++) { // add Char from ArrayChar into String
+            cadena3 += nombre3[i];
+        }
+        System.out.println(cadena3); // Show CharArray into String
+
+    }
+
+}
--- ProgramasJavaDAM/src/ordenamiento/MergeSort.java
+++ ProgramasJavaDAM/src/ordenamiento/MergeSort.java
@@ -1,5 +1,8 @@
 package ordenamiento;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class MergeSort {
 
     public static int[] mergeSort(int array[])
@@ -107,5 +110,50 @@ public class MergeSort {
 
     public static void main(String[] args) {
 
+        // Creamos el array de hombres -->
+        String[] hombres = { "Alejandro", "Carlos", "Javier", "Diego", "Pablo", "Sergio", "David", "Rubén", "Manuel",
+                "Adrián", "Iván", "Óscar", "Raúl", "Álvaro", "Mario", "Tomás", "Samuel", "Gonzalo", "Hugo", "Mateo",
+                "Bruno", "Nicolás", "Lucas", "Martín", "Rafael", "Fernando", "Jorge", "Luis", "Enrique", "Víctor",
+                "Jaime",
+                "Andrés", "Eduardo", "Iker", "Saúl", "Ángel", "Cristian", "Marcos", "Joel", "Sebastián", "Thiago",
+                "Gael",
+                "Adriel", "Damián", "Elías", "Max", "Leo", "Héctor" };
+
+        // Creamos el array de mujeres -->
+        String[] mujeres = { "María", "Lucía", "Sofía", "Laura", "Elena", "Ana", "Carmen", "Paula", "Claudia", "Irene",
+                "Sara", "Marta", "Julia", "Noelia", "Natalia", "Alicia", "Beatriz", "Rocío", "Patricia", "Eva",
+                "Verónica",
+                "Daniela", "Andrea", "Marina", "Isabel", "Teresa", "Lidia", "Nerea", "Ariadna", "Silvia", "Lorena",
+                "Ainhoa", "Esther", "Alba", "Mónica", "Pilar", "Belén", "Helena", "Jimena", "Olga", "Amaya", "Carla",
+                "Manuela", "Violeta", "Luna", "Valeria", "Emma", "Clara" };
+
+        // Creamos el array de 100 posiciones -->
+        String[] nombresMezclados = new String[100];
+        Random random = new Random();
+
+        // Rellenamos el array nuevo -->
+        for (int i = 0; i < nombresMezclados.length; i++) {
+
+            // Decidimos al azar si toca hombre o mujer -->
+            int genero = random.nextInt(2);
+
+            // Elegimos un nombre al azar de la lista correspondiente -->
+            int indiceNombre = random.nextInt(10);
+
+            // ifelse para asignar valores al array -->
+            if (genero == 0)
+                nombresMezclados[i] = hombres[indiceNombre];
+            else
+                nombresMezclados[i] = mujeres[indiceNombre];
+
+        }
+
+        // Mostramos el resultado -->
+        System.out.println(Arrays.toString(nombresMezclados));
+
+        // Ordenamos la cadena -->
+
+        // Mostramos la cadena corregida -->
+        System.out.println(Arrays.toString(nombresMezclados));
     }
 }
