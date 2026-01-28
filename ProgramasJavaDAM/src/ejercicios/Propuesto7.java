608220a36ce90ca9206dfa1dddbbcc42ad7f9c05
Hector <hmorenoveleiro@gmail.com>
2026-01-28 14:10:21 +0100

    .

--- ProgramasJavaDAM/src/ejercicios/Propuesto7.java
+++ ProgramasJavaDAM/src/ejercicios/Propuesto7.java
@@ -41,6 +41,13 @@ public class Propuesto7 {
     }
 
     public static int[] fusionarYOrdenar(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+
+        System.arraycopy(a, 0, c, 0, a.length);
+
+        System.arraycopy(b, 0, c, a.length, b.length);
+
+        Arrays.sort(c);
 
         return c;
     }
