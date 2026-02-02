7bcdf5072c1520b92c6b1096416fa50b76de4464
Hector <hmorenoveleiro@gmail.com>
2026-02-02 09:00:17 +0100

    .

--- ProgramasJavaDAM/src/ejercicios/Propuesto8.java
+++ ProgramasJavaDAM/src/ejercicios/Propuesto8.java
@@ -1,6 +1,7 @@
 package ejercicios;
 
 import java.util.Random;
+import java.util.Arrays;
 
 public class Propuesto8 {
 
@@ -12,6 +13,20 @@ public class Propuesto8 {
         for (int i = 0; i < aleatorios.length; i++) {
             aleatorios[i] = random.nextInt(500);
         }
+
+        System.out.println(Arrays.toString(aleatorios));
+
+        // opcion1 -->
+        Arrays.sort(aleatorios);
+
+        // genero lista con 10 +altos -->
+        int[] altos = new int[10];
+
+        altos = Arrays.copyOfRange(aleatorios, 990, 999);
+        System.out.println("Los 10 mas altos son --> ");
+        System.out.println(Arrays.toString(aleatorios));
+
+        
     }
 
 }
