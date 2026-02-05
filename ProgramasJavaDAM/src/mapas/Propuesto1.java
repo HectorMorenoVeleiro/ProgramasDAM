2b07896f8eaa1cf15f16654f15ed68820b776796
Hector <hmorenoveleiro@gmail.com>
2026-02-05 11:54:25 +0100

    .

---
+++ ProgramasJavaDAM/src/mapas/Propuesto1.java
@@ -0,0 +1,20 @@
+package mapas;
+
+public class Propuesto1 {
+
+    /*
+     * Ejercicio6
+     * Implementa el control de acces al area restringida de un programa. Se debe
+     * pedir un nombre de usuario y una contraseña. Si el usuario introduce los
+     * datos correctamente, el programa dira "Ha accedido al area restringida". El
+     * usuario tendra un maximo de 3 oportunidades. Si se agotan las oportunidades
+     * el porograma dira " Lo siento, no tiene acceso al area restringida". Los
+     * nombres de usuario con sus correspondientes contraseñas deben estar
+     * almacenados en una estructura de la clase HashMap.
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+}
