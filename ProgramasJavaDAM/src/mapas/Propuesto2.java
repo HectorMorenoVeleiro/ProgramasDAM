e87f98433e8e13a96b85541b4fbd0005ef7845f1
Hector <hmorenoveleiro@gmail.com>
2026-02-05 13:01:59 +0100

    .

--- ProgramasJavaDAM/src/mapas/Propuesto2.java
+++ ProgramasJavaDAM/src/mapas/Propuesto2.java
@@ -1,4 +1,42 @@
 package mapas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Propuesto2 {
+
+    /*
+     * Ejercicio10
+     * Crea un mini-diccionario de palabras traducidas del español al ingles y
+     * viceversa
+     * Con su correspondiente traducción ( hashmap )
+     */
+    static void main(String[] args) {
+
+        HashMap<String, String> mapDiccionario = new HashMap<>();
+
+        mapDiccionario.put("gato", "cat");
+        mapDiccionario.put("perro", "dog");
+        mapDiccionario.put("casa", "house");
+        mapDiccionario.put("libro", "book");
+        mapDiccionario.put("coche", "car");
+        mapDiccionario.put("árbol", "tree");
+        mapDiccionario.put("comida", "food");
+        mapDiccionario.put("agua", "water");
+        mapDiccionario.put("fuego", "fire");
+        mapDiccionario.put("cielo", "sky");
+
+        System.out.println("--- contenido del diccionario ---");
+        for (Map.Entry<String, String> entrada : mapDiccionario.entrySet()) {
+            System.out.println(entrada.getKey() + " en ingles es: " + entrada.getValue());
+        }
+
+        System.out.println(mapDiccionario.keySet());
+
+        String[] palabras
+        // buscar palabra especifica
+
+        String buscar
+
+    }
 }
