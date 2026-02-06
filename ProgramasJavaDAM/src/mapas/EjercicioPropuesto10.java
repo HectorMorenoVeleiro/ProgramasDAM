4269b47782bf3f985df238b3bc23261f5b6ca27c
Hector <hmorenoveleiro@gmail.com>
2026-02-06 10:35:03 +0100

    .

---
+++ ProgramasJavaDAM/src/mapas/EjercicioPropuesto10.java
@@ -0,0 +1,26 @@
+package mapas;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class EjercicioPropuesto10 {
+
+    /*
+     * 10.
+     * Realiza un programa que cree un array de 100 posiciones con numeros
+     * aleatorios entre 1 y 100. Una vez creado el array el programa debera
+     * ordenarlo y mostrar los numeros entre 1 y 100 que no han aparecido.
+     */
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] array = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1 + random.nextInt(99);
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
