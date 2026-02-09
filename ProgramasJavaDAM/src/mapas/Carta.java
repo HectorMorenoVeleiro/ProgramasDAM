ab7d395931f98728f48d8923aef3046535cc3a0c
Hector <hmorenoveleiro@gmail.com>
2026-02-09 09:38:21 +0100

    .

--- ProgramasJavaDAM/src/mapas/Carta.java
+++ ProgramasJavaDAM/src/mapas/Carta.java
@@ -27,13 +27,25 @@ public class Carta {
         this.palo = palos[random.nextInt(palos.length)].name();
     }
 
-    // getters y setters
     public String getValor() {
         return valor;
     }
 
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
     public String getPalo() {
         return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        return "Carta [valor=" + valor + ", palo=" + palo + "]";
     }
 
     public static void main(String[] args) {
