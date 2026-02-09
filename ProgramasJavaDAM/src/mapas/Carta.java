a5cff305663ae56ccfba98e61ac960ae64d88f1b
Hector <hmorenoveleiro@gmail.com>
2026-02-09 09:36:44 +0100

    .

--- ProgramasJavaDAM/src/mapas/Carta.java
+++ ProgramasJavaDAM/src/mapas/Carta.java
@@ -15,14 +15,14 @@ public class Carta {
     }
 
     // atributos -->
-    Valor[] valores = Valor.values();
-    Palos[] palos = Palos.values();
     String valor;
     String palo;
 
     // constructor -->
     Carta() {
         Random random = new Random();
+        Valor[] valores = Valor.values();
+        Palos[] palos = Palos.values();
         this.valor = valores[random.nextInt(valores.length)].name();
         this.palo = palos[random.nextInt(palos.length)].name();
     }
@@ -32,22 +32,8 @@ public class Carta {
         return valor;
     }
 
-    public void setValor(String valor) {
-        for (int i = 0; i < valores.length; i++) {
-            valor = valores[i].name();
-            this.valor = valor;
-        }
-    }
-
     public String getPalo() {
         return palo;
-    }
-
-    public void setPalo(String palo) {
-        for (int i = 0; i < palos.length; i++) {
-            palo = palos[i].name();
-            this.palo = palo;
-        }
     }
 
     public static void main(String[] args) {
@@ -59,6 +45,9 @@ public class Carta {
             System.out.println(carta);
             mano.add(carta);
         }
+
+        System.out.println("ve tu mano");
+        System.out.println(mano);
     }
 
 }
