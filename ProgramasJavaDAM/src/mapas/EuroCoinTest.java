bc067ed300b785e57dbbe1737d6e9dee0c06c89f
Hector <hmorenoveleiro@gmail.com>
2026-02-20 09:11:29 +0100

    .

--- ProgramasJavaDAM/src/mapas/EuroCoinTest.java
+++ ProgramasJavaDAM/src/mapas/EuroCoinTest.java
@@ -1,5 +1,7 @@
 package mapas;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class EuroCoinTest {
@@ -7,23 +9,18 @@ public class EuroCoinTest {
     public static void main(String[] args) {
 
         Random random = new Random(); // creas random
-        EuroCoin[] monedas = new EuroCoin[6]; // creas array de monedas
+        List<EuroCoin> monedas = new ArrayList<EuroCoin>();
         float prob = 0; // creas una probabilidad a 0
 
-        for (int i = 0; i < monedas.length; i++) { // for para crear cada moneda
-            if (i == 0) // instruccion condicional para excepcion de primera instancia
-                monedas[i] = new EuroCoin();
-            else { // instruccion condicional que recoge uno de los dos valores previos del del
-                   // array
-                   // segun una probabilidad generada por un random
-                monedas[i] = new EuroCoin();
-                prob = random.nextFloat();
-                if (prob > 0.5) {
-                    monedas[i].setValor(monedas[i - 1].getValor());
-                } else {
-                    monedas[i].setPosicion(monedas[i - 1].getPosicion());
-                }
-            }
+        for (int i = 0; i < 6; i++) { // for para crear cada moneda
+            monedas.add(new EuroCoin());
+            prob = random.nextFloat();
+            if (i != 0) // instruccion condicional que recoge uno de los dos valores previos del del
+                // array segun una probabilidad generada por un random
+                if (prob > 0.5)
+                    monedas.get(i).setValor(monedas.get(i - 1).getValor());
+                else
+                    monedas.get(i).setPosicion(monedas.get(i - 1).getPosicion());
         }
 
         for (EuroCoin moneda : monedas) // muestras el condenido del array monedas
