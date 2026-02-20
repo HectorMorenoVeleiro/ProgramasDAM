57df83317ac10c5506c77304559ff362cb8655f7
Hector <hmorenoveleiro@gmail.com>
2026-02-20 10:36:46 +0100

    .

--- ProgramasJavaDAM/src/mapas/EuroCoin.java
+++ ProgramasJavaDAM/src/mapas/EuroCoin.java
@@ -24,9 +24,9 @@ public class EuroCoin {
 
     // atributes
 
-    String[] valores = { "1 centimo", "2 centimos", "5 centimos", "10 centimos", "20 centimos", "50 centimos", "1 euro",
-            "2 euros" };
-    String[] posiciones = { "cara", "cruz" };
+    static String[] valores = { "1 centimo", "2 centimos", "5 centimos", "10 centimos", "20 centimos", "50 centimos",
+            "1 euro", "2 euros" };
+    static String[] posiciones = { "cara", "cruz" };
 
     String posicion;
     String valor;
