535be40126f218c373e6d37a7072f8c9aec79e0d
Hector Moreno Veleiro <hmorenoveleiro@gmail.com>
2026-04-07 09:00:13 +0200

    .

---
+++ ProgramasJavaDAM/src/tercer_trimestre/Cuenta.java
@@ -0,0 +1,14 @@
+package tercer_trimestre;
+
+public class Cuenta {
+
+    // atributos -->
+    private float saldo;
+    private int numeroConsignaciones = 0;
+    private int numeroRetiros = 0;
+    private float tasaAnual; // en porcentaje (interes)
+
+    // define comision mensual cobrada a cuenta bancaria -->
+    protected float comisionMensual = 0;
+
+}
