215fba524af1f4926d7164af219551fd55ff15d7
Hector Moreno Veleiro <hmorenoveleiro@gmail.com>
2026-04-16 11:49:52 +0200

    .

---
+++ ProgramasJavaDAM/src/tercer_trimestre/tienda_mascotas/Mascota.java
@@ -0,0 +1,10 @@
+package tercer_trimestre.tienda_mascotas;
+
+public class Mascota {
+
+    // todos los animales tienen nombre, edad y color
+    protected String nombre;
+    protected String color;
+    protected int edad;
+
+}
