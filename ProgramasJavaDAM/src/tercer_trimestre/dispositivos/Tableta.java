902c7b1214616c613cbef1a41ab470b5ba267e61
hector Moreno Veleiro <hmorenoveleiro@gmail.com>
2026-04-20 09:29:11 +0200

    .

--- ProgramasJavaDAM/src/tercer_trimestre/dispositivos/Tableta.java
+++ ProgramasJavaDAM/src/tercer_trimestre/dispositivos/Tableta.java
@@ -2,4 +2,11 @@ package tercer_trimestre.dispositivos;
 
 public class Tableta {
 
+    String comida;
+    String perro;
+
+    public Tableta(String comida, String perro) {
+        this.comida = comida;
+        this.perro = perro;
+    }
 }
--- ProgramasJavaDAM/src/tercer_trimestre/dispositivos/TestDispositivo.java
+++ ProgramasJavaDAM/src/tercer_trimestre/dispositivos/TestDispositivo.java
@@ -5,7 +5,7 @@ public class TestDispositivo {
     public static void main(String[] args) {
         // creamos tableta DELL
         // que imprimira el constructor
-        Tableta tableta = new Tableta("Dell");
+        Tableta tableta = new Tableta("Dell", "Aaubduyawgbyduawol");
         // que imprima el toString
         System.out.println(tableta);
     }
