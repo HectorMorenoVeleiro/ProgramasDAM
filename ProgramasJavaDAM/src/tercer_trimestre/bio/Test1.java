c7f7519f0f776c44565985b046ad57e245e447f6
hector Moreno Veleiro <hmorenoveleiro@gmail.com>
2026-04-23 09:34:33 +0200

    .

--- ProgramasJavaDAM/src/tercer_trimestre/bio/Test1.java
+++ ProgramasJavaDAM/src/tercer_trimestre/bio/Test1.java
@@ -9,20 +9,33 @@ public class Test1 {
         Canido mafalda = new Perro("guff", "salchichas", "pisero", "mafaldus galaicus");
         Animal feroz = new Lobo(null, null, null, null);
         Felino scar = new Leon(null, null, null, null);
+        Animal conbotas = new Gato(null, null, null, null);
 
         System.out.println(mafalda.toString());
         System.out.println(feroz.toString());
         System.out.println(scar.toString());
+        System.out.println(conbotas.toString());
 
         // UPCASTING (en el segundo da TRUE porque termina siendo un animal)
         System.out.println(mafalda instanceof Perro); // TRUE
         System.out.println(feroz instanceof Lobo); // FALSE (es lobo | es canido)
         System.out.println(scar instanceof Leon);
+        System.out.println(conbotas instanceof Gato);
 
         /*
          * la previa no era relevante (feroz instanceof Animal) porque efectivamente es
          * un animal, mientras que la segunda no
          */
+
+        Perro pe = (Perro) mafalda;
+        Lobo lo = (Lobo) feroz;
+        Canido ca = (Canido) feroz;
+        Felino fe = (Felino) feroz;
+
+        System.out.println(pe);
+        System.out.println(lo);
+        System.out.println(ca);
+        System.out.println(fe);
 
     }
 }
